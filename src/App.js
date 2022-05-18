95fbc905498a731cd2ceb353a374aec68f8d2e4c
Ashley Hayes <ashley_hayes@hotmail.ca>
2022-05-17 23:03:58 -0600

    built main components of application to meet all technical requirements. Added a form for entering text prompts, submitting form sends a prompt to the OpenAI API. Results are all being displayed in a list, sorted from newest to oldest and includes both the prompt and response. Also added the optional feature of saving responses if user leaves or reloads page using localStorage.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import OpenAI from 'openai-api';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [cards, setCards] = useState([]);
+  
+  const OPENAI_API_KEY = process.env.REACT_APP_OPENAI;
+  const openai = new OpenAI(OPENAI_API_KEY);
+
+  useEffect(() => {
+    const localStoredQueries = localStorage.getItem('storedQueries');
+
+    if (localStoredQueries) {
+      setCards(JSON.parse(localStoredQueries));
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const gptResponse = await openai.complete({
+        engine: 'text-curie-001',
+        prompt: input,
+        maxTokens: 100,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+    });
+
+    setCards([
+      {
+        id: gptResponse.data.id,
+        prompt: input,
+        response: gptResponse.data.choices[0].text
+      },
+      ...cards]
+    );
+    
+    localStorage.setItem('storedQueries', JSON.stringify([
+      {
+        id: gptResponse.data.id,
+        prompt: input,
+        response: gptResponse.data.choices[0].text
+      },
+      ...cards]
+    ));
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <textarea name="prompt" placeholder='Enter a prompt here...' onChange={e => setInput(e.target.value)}></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+      <section>
+        {cards.map((item) => {
+          return (
+            <div key={item.id}>
+              <p>{item.prompt}</p>
+              <p>{item.response}</p>
+            </div>
+          )})
+        }
+      </section>
     </div>
   );
 }
